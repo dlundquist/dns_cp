762c63d58587d2c0aacda0dd72e8eff3bc4fdb07
Dustin Lundquist <dustin@null-ptr.net>
2010-07-02 20:33:04 -0700

    Fetch records via zone transfer

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,6 +1,13 @@
 class Zone < ActiveRecord::Base
   validates_uniqueness_of :name
 
+  require 'net/dns/resolver'
+
   def records
+    res = Net::DNS::Resolver.new(:nameservers => "127.0.0.1")
+
+    response = res.axfr(self.name)
+
+    response.answer
   end
 end
